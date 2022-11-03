6ac19e58933004a3f944ebc50d5a82b41d31a54e
kkris26 <krisnuartha09@gmail.com>
2022-11-03 16:45:35 +0800

    uts2

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
     $.ajax({
-        url: "https://dekontaminasi.com/api/id/covid19/stats",
-        type : "GET",
-        success: function(result){
-            console.log(result)
-        }
-    })
+      url: "API/stats.json",
+      method: "GET",
+      success: function (result) {
+        var infected = result.numbers.infected
+        var recovered = result.numbers.recovered
+        var fatal = result.numbers.fatal 
+        // input to html
+        $("#infected").append("<h1>"+infected+" Case</h1>");
+        $("#recovered").append("<h1>"+recovered+" Case</h1>");
+        $("#fatal").append("<h1>"+fatal+" Case</h1>");
+      },
+    });
 })
